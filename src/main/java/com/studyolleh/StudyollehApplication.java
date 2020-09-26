6f2c75468c81cdf8471507e02df2355a99145cc3
sedin <leesejinn@naver.com>
2020-09-26 23:57:07 +0900

    feat: initialize project
    - 프로젝트 초기 셋팅

---
+++ src/main/java/com/studyolleh/StudyollehApplication.java
@@ -0,0 +1,13 @@
+package com.studyolleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyollehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyollehApplication.class, args);
+    }
+
+}
