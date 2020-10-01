6d703a54a6953a8355ac4a2ddaaadd5c4efaa9ec
sedin <leesejinn@naver.com>
2020-10-01 22:05:29 +0900

    feat: sign-up view 추가
    - fill sign up form

--- src/main/java/com/studyolleh/account/AccountController.java
+++ src/main/java/com/studyolleh/account/AccountController.java
@@ -9,7 +9,7 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
-        model.addAttribute("signUPForm", new SignUpForm());
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
