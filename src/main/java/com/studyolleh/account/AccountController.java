ef60e916e7828ca5621fa4b47b8d80af56201644
sedin <leesejinn@naver.com>
2020-10-02 01:56:43 +0900

    refactor: AccountController의 로직 Service로 분리
    - AccountController에 있던 회원가입 처리 로직을 Service로 분리

--- src/main/java/com/studyolleh/account/AccountController.java
+++ src/main/java/com/studyolleh/account/AccountController.java
@@ -1,9 +1,6 @@
 package com.studyolleh.account;
 
-import com.studyolleh.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,8 +16,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -38,23 +34,9 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
-        Account account = Account.builder()
-                                 .email(signUpForm.getEmail())
-                                 .nickname(signUpForm.getNickname())
-                                 .password(signUpForm.getPassword()) // TODO encoding 필요
-                                 .studyCreatedByWeb(true)
-                                 .studyEnrollmentResultByWeb(true)
-                                 .studyUpdatedByWeb(true)
-                                 .build();
-        Account newAccount = accountRepository.save(account);
 
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
-                "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
+
         return "redirect:/";
     }
 }
