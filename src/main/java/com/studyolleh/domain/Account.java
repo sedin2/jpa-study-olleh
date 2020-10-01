27093655df91b25a127771836c1c9b3f3b632377
sedin <leesejinn@naver.com>
2020-10-02 01:17:37 +0900

    feat: MailSender 추가
    - 이메일 보내기 위해 MailSender 임시 추가

--- src/main/java/com/studyolleh/domain/Account.java
+++ src/main/java/com/studyolleh/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -55,4 +56,8 @@ public class Account {
     private boolean studyUpdatedByEmail;
 
     private boolean studyUpdatedByWeb;
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
 }
