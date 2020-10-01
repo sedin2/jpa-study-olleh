aea9b7458dd84c03c95192f0e498d37a92444279
sedin <leesejinn@naver.com>
2020-10-02 02:20:55 +0900

    feat: Password Encoding 추가
    - 회원가입 시 password를 encode 하여 저장
    - bcrypt algorithm, salt

--- src/test/java/com/studyolleh/account/AccountControllerTest.java
+++ src/test/java/com/studyolleh/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyolleh.account;
 
+import com.studyolleh.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -62,6 +63,10 @@ class AccountControllerTest {
                         .with(csrf()))
                         .andExpect(status().is3xxRedirection())
                         .andExpect(view().name("redirect:/"));
+
+        Account account = accountRepository.findByEmail("sedin@kakao.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
 
         assertTrue(accountRepository.existsByEmail("sedin@kakao.com"));
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
