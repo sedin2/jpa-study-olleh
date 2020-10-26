4707324b4598bd38b423b7901fcd33fa60446d1c
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-10-26 23:30:47 +0900

    test: login and logout test code 추가
    - 유효한 값일 때, 유효하지 않은 값일 때 나누어 테스트 코드 작성

---
+++ src/test/java/com/studyolleh/main/MainControllerTest.java
@@ -0,0 +1,96 @@
+package com.studyolleh.main;
+
+import com.studyolleh.account.AccountRepository;
+import com.studyolleh.account.AccountService;
+import com.studyolleh.account.SignUpForm;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void addAccount() {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("mocha");
+        signUpForm.setEmail("mocha@email.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+    }
+
+    @AfterEach
+    void deleteAccount() {
+        accountRepository.deleteAll();
+    }
+
+    @DisplayName("이메일로 로그인 성공")
+    @Test
+    void login_with_email() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username", "mocha@email.com")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("mocha"));
+    }
+
+    @DisplayName("닉네임으로 로그인 성공")
+    @Test
+    void login_with_nickname() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username", "mocha")
+                .param("password", "12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("mocha"));
+    }
+
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("username", "asdfasdf")
+                .param("password", "asdfasdf")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+
+    @WithMockUser
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception {
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+    }
+}
