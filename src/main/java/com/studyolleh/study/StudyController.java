00be43865ae88c7bc95848e33eed3ca56ff15043
sedin <leesejinn@naver.com>
2020-11-20 19:03:54 +0900

    feat: Study 개설 기능 추가
    - study 개설에 필요한 view 작성
    - study 개설 기능 구현

---
+++ src/main/java/com/studyolleh/study/StudyController.java
@@ -0,0 +1,52 @@
+package com.studyolleh.study;
+
+import com.studyolleh.account.CurrentUser;
+import com.studyolleh.domain.Account;
+import com.studyolleh.domain.Study;
+import com.studyolleh.study.form.StudyForm;
+import com.studyolleh.study.validator.StudyFormValidator;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequiredArgsConstructor
+public class StudyController {
+
+    private final StudyService studyService;
+    private final ModelMapper modelMapper;
+    private final StudyFormValidator studyFormValidator;
+
+    @InitBinder("studyForm")
+    public void studyFormInitBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(studyFormValidator);
+    }
+
+    @GetMapping("/new-study")
+    public String newStudyForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new StudyForm());
+        return "study/form";
+    }
+
+    @PostMapping("/new-study")
+    public String newStudySubmit(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors) throws UnsupportedEncodingException {
+        if (errors.hasErrors()) {
+            return "study/form";
+        }
+
+        Study newStudy = studyService.createNewStudy(modelMapper.map(studyForm, Study.class), account);
+        return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), String.valueOf(StandardCharsets.UTF_8));
+    }
+}
---
+++ src/main/java/com/studyolleh/study/StudyService.java
@@ -0,0 +1,21 @@
+package com.studyolleh.study;
+
+import com.studyolleh.domain.Account;
+import com.studyolleh.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+
+    public Study createNewStudy(Study study, Account account) {
+        Study newStudy = studyRepository.save(study);
+        newStudy.addManager(account);
+        return newStudy;
+    }
+}
